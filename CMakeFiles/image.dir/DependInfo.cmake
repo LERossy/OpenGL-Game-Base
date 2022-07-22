e5d714fb0603dc0a0fdadf5c79ca0e853f1990c2
LERossy <84641124+LERossy@users.noreply.github.com>
2022-07-22 10:48:39 -0400

    Add files via upload

---
+++ CMakeFiles/image.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/aelhossini/Laila/OpenGL/glad/src/glad.c" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/home/aelhossini/Laila/OpenGL/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "glfw"
+  "/home/aelhossini/Laila/OpenGL/glad/include"
+  "/home/aelhossini/Laila/OpenGL/freetype-2.4.0/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/Image_Generator.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/Image_Generator.cpp.o"
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/Object_Rend.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/Object_Rend.cpp.o"
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/Resource_Manager.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/Resource_Manager.cpp.o"
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/Scene.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/Scene.cpp.o"
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/Shader.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/Shader.cpp.o"
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/Texture.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/Texture.cpp.o"
+  "/home/aelhossini/Laila/OpenGL/Virtek_Project/stb_image.cpp" "/home/aelhossini/Laila/OpenGL/Virtek_Project/CMakeFiles/image.dir/stb_image.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "glfw"
+  "/home/aelhossini/Laila/OpenGL/glad/include"
+  "/home/aelhossini/Laila/OpenGL/freetype-2.4.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/image.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image.dir/Image_Generator.cpp.o"
+  "CMakeFiles/image.dir/home/aelhossini/Laila/OpenGL/glad/src/glad.c.o"
+  "CMakeFiles/image.dir/Shader.cpp.o"
+  "CMakeFiles/image.dir/Texture.cpp.o"
+  "CMakeFiles/image.dir/Resource_Manager.cpp.o"
+  "CMakeFiles/image.dir/Scene.cpp.o"
+  "CMakeFiles/image.dir/Object_Rend.cpp.o"
+  "CMakeFiles/image.dir/stb_image.cpp.o"
+  "image.pdb"
+  "image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
